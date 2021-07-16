04348b0cfcb5169804e3ad1dd351d3466ac1da26
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-07-16 13:20:02 -0400

    added player class that will be the parent class of user and oppoenent

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.opponent = player2
+        
+    def choice(self):   
+        self.choice = []
