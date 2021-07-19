8e58a7155689d28551281047636e25f1883443af
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-07-19 13:13:56 -0400

    refined code in player file to contain logic check for gestures and choice

--- human.py
+++ human.py
@@ -1,21 +1,16 @@
-from player import Player
+from players import Player
 
 class Human(Player):
     def __init__(self, player1):
-        self.player1 = ''
+        self.player1 = player1
         print('this works')
 
-    def player_choice(self):
-        self.choice = input('Please choose: R for rock, P for paper, S for scissors, L for lizzard, and SP for Spock ').toLowerCase()
-        while self.choice is not ['r', 's', 'p', 'l', 'sp']:
-            print('sorry invalid choice! Please select r, s, p, l, or sp!')
-            self.choice = input('Please choose: R for rock, P for paper, S for scissors, L for lizzard, and SP for Spock ').toLowerCase()
-            
-            return self.choice
-        
-        print(self.choice)
-        return self.choice
+    def player_choice(self, getstures):
+        self.choice = input('Please choose: rock, paper, scissors, lizard, or Spock ').lower()
+        if self.choice in getstures:
+            print(self.choice)
+        else:
+            while self.choice not in getstures:
+                print('sorry invalid choice! Please select rock, scissors, paper, lizzard or spock')
+                choice = input('Please choose: rock, paper, scissors, lizard, or Spock ').lower()
 
-test = player_choice()
-
-print(test)
