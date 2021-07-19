e14de7f6cc3bb87749a00462adf2e45047b455c0
n-rdy <giuseppegelardi@icloud.com>
2021-07-19 12:47:27 -0500

    restructuring classes

--- user.py
+++ user.py
@@ -13,3 +13,19 @@ User.name = "player1"
 User(User.name, User.player_moves)
 print(User.name)
 print(User.player_moves)
+
+
+
+# TODO impliment the following
+
+# self.choice = input('Please choose: rock, paper, scissors, lizard, or Spock ').lower()
+# if self.choice in getstures:
+#     print(self.choice)
+# else:
+#     while self.choice not in getstures:
+#         print('sorry invalid choice! Please select rock, scissors, paper, lizzard or spock')
+#         self.choice = input('Please choose: rock, paper, scissors, lizard, or Spock ').lower()
+#
+# test = Human('human')
+#
+# test.player_choice()
